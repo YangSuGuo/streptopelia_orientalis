8056dfe6c18f4879e9e0f5b3d7ef5af342a25039
YSG <Dr.ysg@qq.com>
2026-01-08 22:23:38 +0800

    refactor(dao): 简化数据库访问对象实现
    - 在 build.yaml 中启用 data_class_to_companions 选项
    - 移除 CategoryDao 中的手动 CRUD 方法实现
    - 移除 RecordDao 中的手动 CRUD 方法实现
    - 移除 RecordPhotoDao 中的手动 CRUD 方法实现
    - 移除 RecordStepDao 中的手动 CRUD 方法实现
    - 移除 RecordTypeDao 中的手动 CRUD 方法实现
    - 移除 RecordValueDao 中的手动 CRUD 方法实现
    - 为所有 DAO 类添加对应的表导入语句
    - 使用 super.db 简化构造函数实现

--- lib/data/drift/dao/category_dao.dart
+++ lib/data/drift/dao/category_dao.dart
@@ -1,29 +1,11 @@
 import 'package:drift/drift.dart';
 import '../app_database.dart';
+import '../tables/categories.dart';
 
 part 'category_dao.g.dart';
 
 @DriftAccessor(tables: [Categories])
 class CategoryDao extends DatabaseAccessor<AppDatabase> with _$CategoryDaoMixin {
-  CategoryDao(AppDatabase db) : super(db);
+  CategoryDao(super.db);
 
-  Future<List<Category>> getAllCategories() {
-    return select(categories).get();
-  }
-
-  Future<Category?> getCategoryById(int id) {
-    return (select(categories)..where((tbl) => tbl.id.equals(id))).getSingleOrNull();
-  }
-
-  Future<int> insertCategory(Insertable<Category> category) {
-    return into(categories).insert(category);
-  }
-
-  Future<void> updateCategory(CategoryUpdates category) {
-    return update(categories).replace(category);
-  }
-
-  Future<void> deleteCategory(int id) {
-    return (delete(categories)..where((tbl) => tbl.id.equals(id))).go();
-  }
 }
--- lib/data/drift/dao/record_step_dao.dart
+++ lib/data/drift/dao/record_step_dao.dart
@@ -1,41 +1,11 @@
 import 'package:drift/drift.dart';
 import '../app_database.dart';
+import '../tables/record_steps.dart';
 
 part 'record_step_dao.g.dart';
 
 @DriftAccessor(tables: [RecordSteps])
 class RecordStepDao extends DatabaseAccessor<AppDatabase> with _$RecordStepDaoMixin {
-  RecordStepDao(AppDatabase db) : super(db);
+  RecordStepDao(super.db);
 
-  Future<List<RecordStep>> getAllRecordSteps() {
-    return select(recordSteps).get();
-  }
-
-  Future<RecordStep?> getRecordStepById(int id) {
-    return (select(recordSteps)..where((tbl) => tbl.id.equals(id))).getSingleOrNull();
-  }
-
-  Future<int> insertRecordStep(Insertable<RecordStep> recordStep) {
-    return into(recordSteps).insert(recordStep);
-  }
-
-  Future<void> updateRecordStep(RecordStepUpdates recordStep) {
-    return update(recordSteps).replace(recordStep);
-  }
-
-  Future<void> deleteRecordStep(int id) {
-    return (delete(recordSteps)..where((tbl) => tbl.id.equals(id))).go();
-  }
-
-  // Get record steps by record ID
-  Future<List<RecordStep>> getRecordStepsByRecordId(int recordId) {
-    return (select(recordSteps)..where((tbl) => tbl.recordId.equals(recordId))).get();
-  }
-
-  // Get completed record steps by record ID
-  Future<List<RecordStep>> getCompletedRecordStepsByRecordId(int recordId) {
-    return (select(recordSteps)
-          ..where((tbl) => tbl.recordId.equals(recordId) & tbl.stepStatus.equals('completed')))
-        .get();
-  }
 }
